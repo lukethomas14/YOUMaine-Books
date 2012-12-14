8e94db124ddf6e603e29d3412212f93ad837f27d
Luke Thomas <me@lukethomas.com>
2012-12-13 20:29:51 -0500

    scoped temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Umainebook::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-    resources :books
+    resources :books, :only => [:show]
 
   # Sample resource route with options:
   #   resources :products do
