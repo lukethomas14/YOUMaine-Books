e7745999f618b2d92f1669742b81f63a69285be3
Luke Thomas <me@lukethomas.com>
2012-12-13 20:31:41 -0500

    removed scoping

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Umainebook::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-    resources :books, :only => [:show]
+    resources :books
 
   # Sample resource route with options:
   #   resources :products do
