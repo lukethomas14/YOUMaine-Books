c97a97a54059523bb294064e9be237055e3ebc5a
Luke Thomas <me@lukethomas.com>
2012-12-16 12:49:31 -0500

    captcha mod

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
 	def create 
 		@book = Book.new(params[:book])	
 		
-		if verify_recaptcha && @book.save
+		if verify_recaptcha(:model => @book, :message => "Please enter the correct captcha!") && @book.save
 			redirect_to @book, notice: 'Book was successfully created.'
 
 		else flash.now[:error] = "There was a problem with the info you supplied"
